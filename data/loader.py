0b367bf02c016de13acea56e79d32d6ab863123d
Husen Komilov <117765497+HusenKomilov@users.noreply.github.com>
2022-12-16 22:21:37 +0500

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -5,7 +5,7 @@ from telebot import TeleBot
 from telebot.types import BotCommand
 from config import TOKEN
 
-bot = TeleBot('5890013686:AAGHTekD98t5hHITe_FI254ln0yRD2tKxC4', use_class_middlewares=True, parse_mode='html')
+bot = TeleBot(TOKEN, use_class_middlewares=True, parse_mode='html')
 
 bot.set_my_commands(commands=[
     BotCommand('/start', "Перезапустите бота🔄")
